6d791451ff225262e2c5da449c8256676e30b298
subhankar <subhankar.biswas@target.com>
2016-10-18 00:00:47 +0530

    Fix test isseu, added version of dependencies

--- app/mod_site/controllers.py
+++ app/mod_site/controllers.py
@@ -3,7 +3,7 @@ from flask import current_app as app
 
 mod_site = Blueprint('site', __name__)
 
-@mod_site.route("/", methods=["GET"])
 
+@mod_site.route("/", methods=["GET"])
 def home():
-    return render_template("index.html", total=0, datasets= [], coreDatasets= [], title= 'home')
+    return render_template("index.html", total=0, datasets=[], coreDatasets=[], title='home')
