3b6635703eeb7b19c83370cc2fa58039fe66fd8d
subhankar <subhankar.biswas@target.com>
2016-11-06 16:58:56 +0530

    [Hot Fix] travis deploy fix

--- zappa_deploy.sh
+++ zappa_deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 current_path=`pwd`
 export AWS_CONFIG_FILE=${current_path}/.credentials
+mkdir /root/.aws
 python ${current_path}/zappa_set_env.py
 status=`zappa update stage -s ${current_path}/zappa_settings_deploy.json`
 echo ${status}
