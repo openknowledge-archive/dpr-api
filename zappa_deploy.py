c5fa6658190322c451583f624127d2d948c12507
subhankar <subhankar.biswas@target.com>
2016-12-20 19:23:47 +0530

    Added createdb back - fixes #153

--- zappa_deploy.py
+++ zappa_deploy.py
@@ -97,5 +97,6 @@ if __name__ == '__main__':
     create_zappa_settings(stage_name=env)
     write_aws_credential()
     run_zappa(env)
+    check_output(['python', 'manager.py', 'createdb'])
     check_output(['python', 'manager.py', 'populate'])
 
