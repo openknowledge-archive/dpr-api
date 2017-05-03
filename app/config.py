f5f96d17021999063c880361e67b5188e8b3c864
zelima <irakli.mchedlishvili@datopian.com>
2017-05-03 12:35:31 +0400

    [swagger][xs]: updated title and version of swagger - refs #330

--- app/config.py
+++ app/config.py
@@ -21,13 +21,11 @@ class BaseConfig(object):
     API_DOCS = '/apidocs/index.html'
     SWAGGER = {
         "swagger_version": "2.0",
-        "title": "DPR API",
+        "title": "DataHub API",
         "specs": [
             {
-                "version": "0.0.1",
-                "title": "v1",
+                "title": "v0.5",
                 "endpoint": 'spec',
-                "description": "First Cut for DPR API",
                 "route": '/api/swagger.json',
                 "rule_filter": lambda rule: True
             }
