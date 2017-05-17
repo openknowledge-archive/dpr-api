ef30a7567a13f480c752a671fb14524f91528566
zelima <irakli.mchedlishvili@datopian.com>
2017-05-18 00:42:55 +0400

    [tests][s]: strip out all unnecessary test fixtures from package/models test (leftover from previous refactoring) - refs #376.

--- tests/package/test_models.py
+++ tests/package/test_models.py
@@ -16,7 +16,8 @@ from app.profile.models import User, Publisher, UserRoleEnum, PublisherUser
 
 
 class PackageTestCase(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setup_class(self):
         self.publisher_one = 'test_publisher1'
         self.publisher_two = 'test_publisher2'
         self.package_one = 'test_package1'
@@ -46,35 +47,14 @@ class PackageTestCase(unittest.TestCase):
             metadata1.tags.append(tag1)
             publisher1.packages.append(metadata1)
 
-            metadata2 = Package(name=self.package_two)
-            tag2 = PackageTag(descriptor=dict(name='test_two'))
-            metadata2.tags.append(tag2)
-            publisher1.packages.append(metadata2)
-
-            metadata3 = Package(name=self.package_one)
-            tag3 = PackageTag(descriptor=dict(name='test_three'))
-            metadata3.tags.append(tag3)
-            publisher2.packages.append(metadata3)
-
-            metadata4 = Package(name=self.package_two)
-            tag4 = PackageTag(descriptor=dict(name='test_four'))
-            metadata4.tags.append(tag4)
-            publisher2.packages.append(metadata4)
-
-            metadata5 = Package(name=self.package_three)
-            tag5 = PackageTag(descriptor=dict(name='test_four'))
-            metadata5.tags.append(tag5)
-            publisher2.packages.append(metadata5)
-
             db.session.add(user1)
             db.session.add(user2)
-
             db.session.commit()
 
     def test_composite_key(self):
         res = Package.query.join(Publisher).filter(Publisher.name ==
                                                    self.publisher_one).all()
-        self.assertEqual(2, len(res))
+        self.assertEqual(1, len(res))
 
     def test_get_by_publisher(self):
         pkg = Package.get_by_publisher(self.publisher_one, self.package_one)
@@ -87,9 +67,9 @@ class PackageTestCase(unittest.TestCase):
     def test_get_by_publisher_returns_none_if_no_package(self):
         pkg = Package.get_by_publisher(self.publisher_one, 'not_a_package')
         self.assertIsNone(pkg)
-        
 
-    def tearDown(self):
+    @classmethod
+    def teardown_class(self):
         with self.app.app_context():
             db.session.remove()
             db.drop_all()
